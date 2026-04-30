568f48efbb8fe8ef23f7f3d1d0381eae5b24c22d
sususie-chen <sususie.chen@outlook.com>
2026-04-30 15:12:24 +0800

    Update boot.ts

--- api/boot.ts
+++ api/boot.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { handle } from "@hono/node-server/vercel";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "./router";
+import { appRouter } from "../server/router";
 
 const app = new Hono();
 
